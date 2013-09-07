15d656bb22601bb518fb8260889f20c7ce689ad5
vagrant <vagrant@precise>
2013-09-07 12:50:02 +0000

    Initial commit

---
+++ tmp/hoge/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hoge::Application.config.secret_key_base = '36b1346b88ec266d900e3d68691eadf3df5804144cb81f2c0c2414f90bda00afe23011ff0940b1a5e8b3465a3c9df511d23b708569e11410718cbd324cb8df90'
---
+++ tmp/my_awesome_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyAwesomeApp::Application.config.session_store :cookie_store, key: '_my_awesome_app_session'
