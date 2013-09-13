a90507fd28a687ceda0b9d54a62225adb733d7df
Yoshihide Chubachi <Yoshihide-chubachi+github@gmail.com>
2013-09-13 21:55:28 +0900

    Fix metadata.rb

--- Berksfile
+++ Berksfile
@@ -1,13 +1,3 @@
 site :opscode
 
 metadata
-cookbook 'locale', github: "hw-cookbooks/locale"
-cookbook 'apt'
-cookbook 'zsh'
-cookbook 'git'
-cookbook 'vim'
-cookbook 'emacs', github: "mattray/emacs"
-cookbook 'ruby_build'
-cookbook 'rbenv', github: "fnichol/chef-rbenv"
-cookbook 'nodejs'
-cookbook 'hub', github: "drnic/chef-hub"
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,18 @@
 name             'precise'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Yoshihide Chubachi'
+maintainer_email 'yoshi@chubachi.net'
 license          'All rights reserved'
-description      'Installs/Configures precise'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+description      'Installs/Configures precise for enPiT'
+# long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'locale', github: "hw-cookbooks/locale"
+depends 'apt'
+depends 'zsh'
+depends 'git'
+depends 'vim'
+depends 'emacs', github: "mattray/emacs"
+depends 'ruby_build'
+depends 'rbenv', github: "fnichol/chef-rbenv"
+depends 'nodejs'
+depends 'hub', github: "drnic/chef-hub"
