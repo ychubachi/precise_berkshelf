20aab4204ce64386b833b4619abd5269fc51a3c0
Yoshihide Chubachi <Yoshihide-chubachi+github@gmail.com>
2013-09-13 22:07:31 +0900

    Reset

--- Berksfile
+++ Berksfile
@@ -1,3 +1,14 @@
 site :opscode
 
 metadata
+
+cookbook 'locale', github: "hw-cookbooks/locale"
+cookbook 'apt'
+cookbook 'zsh'
+cookbook 'git'
+cookbook 'vim'
+cookbook 'emacs', github: "mattray/emacs"
+cookbook 'ruby_build'
+cookbook 'rbenv', github: "fnichol/chef-rbenv"
+cookbook 'nodejs'
+cookbook 'hub', github: "drnic/chef-hub"
--- metadata.rb
+++ metadata.rb
@@ -5,14 +5,3 @@ license          'All rights reserved'
 description      'Installs/Configures precise for enPiT'
 # long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
-depends 'locale', github: "hw-cookbooks/locale"
-depends 'apt'
-depends 'zsh'
-depends 'git'
-depends 'vim'
-depends 'emacs', github: "mattray/emacs"
-depends 'ruby_build'
-depends 'rbenv', github: "fnichol/chef-rbenv"
-depends 'nodejs'
-depends 'hub', github: "drnic/chef-hub"
