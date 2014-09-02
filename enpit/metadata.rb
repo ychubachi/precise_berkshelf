9d1ebf96c12a12ca5b880c4f7e330ad30004accf
Yoshihide Chubachi <yoshi@chubachi.net>
2014-09-02 19:33:48 +0900

    Run berks init

---
+++ enpit/metadata.rb
@@ -0,0 +1,8 @@
+name             'enpit'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures enpit'
+long_description 'Installs/Configures enpit'
+version          '0.1.0'
+
