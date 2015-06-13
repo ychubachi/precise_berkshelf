63f2ea45ca3db000b19eeda03fedf7db7fb03cb6
Yoshihide Chubachi <yoshi@chubachi.net>
2015-06-13 10:29:36 +0900

    Install hub command from the bin package

--- extra_provision.sh
+++ extra_provision.sh
@@ -1,19 +1,19 @@
 # Install software which chef does not install properly.
 
-# Install hub command using system ruby.
-# Chef embeded rake raises exceptions with the receipe.
-sudo apt-get -y install rake bundler
+## Install hub command
 
-git clone git://github.com/github/hub.git
-cd hub
-sudo bundle install
-sudo rake install
-cd ..
-rm -rf hub
+mkdir -p tmp
+cd tmp
+wget https://github.com/github/hub/releases/download/v2.2.1/hub-linux-amd64-2.2.1.tar.gz
+tar zxvf hub-linux-amd64-2.2.1.tar.gz
+cd hub-linux-amd64-2.2.1
+sudo cp ./hub /usr/local/bin
+cd ../..
+rm -rf tmp
 
-# Install ruby gems in rbenv. The rbenv receipe installs them as root.
+## Install ruby gems in rbenv. The rbenv receipe installs them as root.
 gem install bundler rails travis sinatra
 rbenv rehash
 
-# Good bye
+## Good bye
 echo "Please re-login"
